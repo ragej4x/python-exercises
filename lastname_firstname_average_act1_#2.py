7fa8508283a8eb664faf4fe8d629a5f48282f7ea
ragej4x <tongaojimboy@gmail.com>
2024-11-08 20:44:19 +0800

    basics act 1 day 1 to 3

---
+++ lastname_firstname_average_act1_#2.py
@@ -0,0 +1,20 @@
+#Variables
+
+name = str("Jack Cole") #Name
+
+#Grades
+math = float(73)
+science = float(76)
+english = int(75)
+#Calculate grade divide by how many subs
+average = float(math + science + english) / 3
+
+
+#Output
+print(f"Name : {name}")
+
+print(f"Math : {math}")
+print(f"Science : {science}")
+print(f"English : {english}")
+
+print(f"Status : {average}")
